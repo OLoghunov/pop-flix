6ea812b8383e30b92a08810c49b0bcf7a36625f7
OLoghunov <loghunov1@gmail.com>
2025-01-29 23:09:22 +0300

    added many to many relation between users and films tables

--- src/db/models.py
+++ src/db/models.py
@@ -1,9 +1,16 @@
 import uuid
 from datetime import datetime
 from typing import List, Optional
+from enum import Enum
+
 from sqlmodel import SQLModel, Field, Relationship
 from sqlalchemy import Column, ForeignKey
 import sqlalchemy.dialects.postgresql as pg
+
+
+class FilmStatus(str, Enum):
+    WATCHED = "watched"
+    PLANNED = "planned"
 
 
 class UserFilmLink(SQLModel, table=True):
@@ -15,6 +22,13 @@ class UserFilmLink(SQLModel, table=True):
     film_uid: uuid.UUID = Field(
         sa_column=Column(pg.UUID, ForeignKey("films.uid", ondelete="CASCADE"), primary_key=True)
     )
+    status: FilmStatus = Field(
+        sa_column=Column(pg.VARCHAR, nullable=False)
+    )
+    
+    user: "User" = Relationship(back_populates="film_links")
+    film: "Film" = Relationship(back_populates="user_links")
+
 
 
 class Film(SQLModel, table=True):
@@ -23,9 +37,12 @@ class Film(SQLModel, table=True):
     uid: uuid.UUID = Field(
         sa_column=Column(pg.UUID, primary_key=True, default=uuid.uuid4)
     )
+    apiId: int = Field(default=None)
     title: str = Field(nullable=False)
     year: Optional[int] = None
     poster: Optional[str] = None
+    
+    user_links: List["UserFilmLink"] = Relationship(back_populates="film")
     users: List["User"] = Relationship(
         back_populates="films", link_model=UserFilmLink
     )
@@ -48,6 +65,8 @@ class User(SQLModel, table=True):
     passwordHash: str = Field(exclude=True)
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    
+    film_links: List["UserFilmLink"] = Relationship(back_populates="user")
     films: List["Film"] = Relationship(
         back_populates="users", link_model=UserFilmLink
     )
