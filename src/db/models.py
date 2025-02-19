a6c5e47919dfd6c437b0a4a4ef46dcc46470fbee
OLoghunov <loghunov1@gmail.com>
2025-02-19 22:02:49 +0300

    added tmdbId column to a Films model

--- src/db/models.py
+++ src/db/models.py
@@ -38,6 +38,7 @@ class Film(SQLModel, table=True):
         sa_column=Column(pg.UUID, primary_key=True, default=uuid.uuid4)
     )
     apiId: int = Field(default=None)
+    tmdbId: int = Field(default=None)
     title: str = Field(nullable=False)
     year: Optional[int] = None
     poster: Optional[str] = None
